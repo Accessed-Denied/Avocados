771edd08a4f17e20da684f741c930c1a7ccdc76a
sonusainier <rohitsaini@lambdatest.com>
2021-10-25 11:55:47 +0530

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Rohit Saini on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
